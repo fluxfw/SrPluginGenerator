21edc46ecbc250ea218acf4a2934f1d7ea55a73a
Florian Wyss <fw@studer-raimann.ch>
2020-01-10 08:36:11 +0100

    Refactor

--- templates/slots/Services/Cron/CronHook/src/Job/Job.php
+++ templates/slots/Services/Cron/CronHook/src/Job/Job.php
@@ -32,9 +32,7 @@ class Job extends ilCronJob
 
 
     /**
-     * Get id
-     *
-     * @return string
+     * @inheritDoc
      */
     public function getId() : string
     {
@@ -43,7 +41,7 @@ class Job extends ilCronJob
 
 
     /**
-     * @return string
+     * @inheritDoc
      */
     public function getTitle() : string
     {
@@ -52,7 +50,7 @@ class Job extends ilCronJob
 
 
     /**
-     * @return string
+     * @inheritDoc
      */
     public function getDescription() : string
     {
@@ -61,9 +59,7 @@ class Job extends ilCronJob
 
 
     /**
-     * Is to be activated on "installation"
-     *
-     * @return boolean
+     * @inheritDoc
      */
     public function hasAutoActivation() : bool
     {
@@ -72,9 +68,7 @@ class Job extends ilCronJob
 
 
     /**
-     * Can the schedule be configured?
-     *
-     * @return boolean
+     * @inheritDoc
      */
     public function hasFlexibleSchedule() : bool
     {
@@ -83,9 +77,7 @@ class Job extends ilCronJob
 
 
     /**
-     * Get schedule type
-     *
-     * @return int
+     * @inheritDoc
      */
     public function getDefaultScheduleType() : int
     {
@@ -94,20 +86,16 @@ class Job extends ilCronJob
 
 
     /**
-     * Get schedule value
-     *
-     * @return int|array
+     * @inheritDoc
      */
-    public function getDefaultScheduleValue()
+    public function getDefaultScheduleValue()/*:?int*/
     {
         return null;
     }
 
 
     /**
-     * Run job
-     *
-     * @return ilCronJobResult
+     * @inheritDoc
      */
     public function run() : ilCronJobResult
     {
