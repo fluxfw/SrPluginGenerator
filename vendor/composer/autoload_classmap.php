f415ce8cda997a4add0b7482de3d30b38c1718e3
Florian Wyss <fw@studer-raimann.ch>
2020-09-14 11:10:24 +0200

    `Ilias7PreWarn`

--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -83,6 +83,7 @@ return array(
     'srag\\DIC\\SrPluginGenerator\\Database\\PdoStatementContextHelper' => $vendorDir . '/srag/dic/src/Database/PdoStatementContextHelper.php',
     'srag\\DIC\\SrPluginGenerator\\DevTools\\DevToolsCtrl' => $vendorDir . '/srag/dic/src/DevTools/class.DevToolsCtrl.php',
     'srag\\DIC\\SrPluginGenerator\\Exception\\DICException' => $vendorDir . '/srag/dic/src/Exception/DICException.php',
+    'srag\\DIC\\SrPluginGenerator\\Ilias7PreWarn' => $vendorDir . '/srag/dic/src/Ilias7PreWarn.php',
     'srag\\DIC\\SrPluginGenerator\\Loader\\AbstractLoaderDetector' => $vendorDir . '/srag/dic/src/Loader/AbstractLoaderDetector.php',
     'srag\\DIC\\SrPluginGenerator\\Output\\Output' => $vendorDir . '/srag/dic/src/Output/Output.php',
     'srag\\DIC\\SrPluginGenerator\\Output\\OutputInterface' => $vendorDir . '/srag/dic/src/Output/OutputInterface.php',
